c11efb674c87f6c8ca23708c99ba2eff224e19d0
ikhaduki <derewashkina@bk.ru>
2024-12-31 02:02:28 +0300

    Add files via upload

---
+++ Syntax/task1-2.js
@@ -0,0 +1,14 @@
+// let, const, var
+
+/* Автор
+Диана Кук/ikhaduki*/
+const myName=('diana')
+const mySurname =('kuk')
+const myFavoriteDrink =('latte')
+const myFavoriteAnimal =('cat')
+const myNamyFavoriteProgrammingLanguage=('i dont know')
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myName)
---
+++ Syntax/task3-4.js
@@ -0,0 +1,18 @@
+const carName=('SUZUKI')
+let carMaxSpeed=260
+carMaxSpeed=240
+    console.log('У машины изменилась скорость', carMaxSpeed);
+carMaxSpeed=230
+    console.log('У машины изменилась скорость', carMaxSpeed);
+carMaxSpeed=200
+    console.log('У машины изменилась скорость', carMaxSpeed);
+let carOwner=("Anastasia")
+caroOner=('Oleg')
+    console.log('У машины У машины изменился владелец скорость', carOwner);
+carOwner=('Egor')
+    console.log('У машины У машины изменился владелец скорость', carOwner);
+carOwner=('Helga')
+    console.log('У машины У машины изменился владелец скорость', carOwner);
+alert(carName)
+alert(carMaxSpeed)
+alert(carOwner)
