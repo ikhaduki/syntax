6c39f9d4d98099b9e6ae8ebf0098b486a0c3ce31
ikhaduki <derewashkina@bk.ru>
2024-12-31 02:42:53 +0300

    Add files via upload

---
+++ first-project/js-info.js
@@ -0,0 +1,5 @@
+alert("Язык программирования JavaScript разработал Брэндан Эйх в 1995 году, работая в компании Netscape Communications Corporation. ");
+alert("Язык программирования JavaScript был разработан за 10 дней");
+alert("JavaScript появился в 1995 году");
+alert("JavaScript — это не Java");
+alert("Спецификация языка JavaScript называется ECMAScript.");
---
+++ first-project/task2.js
@@ -0,0 +1,8 @@
+console.log('Запускаем таймер');
+alert("5");
+alert("4");
+alert("3");
+alert("2");
+alert("1");
+
+console.log('Обратный отсчет закончен')
